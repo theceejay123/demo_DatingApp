9dd8c8cdee119c372287211d2bea9b7b0c9be90f
Janeal Pimentel <doodlerprizz@hotmail.ca>
2021-05-19 20:38:36 -0500

    Section 7 - Error Handling - client/server

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,9 +23,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(
       (res) => {
         this.router.navigateByUrl('/members');
-      },
-      (err) => {
-        this.toastr.error(err.error);
       }
     );
   }
